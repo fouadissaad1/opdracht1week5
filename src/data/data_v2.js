e60e55922bc708be19e2c24bcb54dcd440d0d090
fouadissaad1 <71667992+fouadissaad1@users.noreply.github.com>
2021-05-16 20:12:03 +0200

    add images + data

---
+++ src/data/data_v2.js
@@ -0,0 +1,62 @@
+export const DATA = [
+    {
+        pageNumber: 1,
+        title: "Tips voor het aanleggen van een moestuin",
+        text: [
+            "Moestuinieren is hip, en wat is er nu leuker dan groenten en fruit eten van eigen kweek? ",
+            "De aanleg van een moestuin is niet moeilijk, ook als je geen of weinig ervaring hiermee hebt. Moestuinieren begint met een goede voorbereiding. Het succes van je moestuin heeft alles te maken met de keuze van de plek, de kwaliteit van de grond, de bemesting en het (in)zaaien.",
+            "Is deze basis goed, dan is de helft van het werk al gedaan.",
+            "---",
+            "[[img/zaaien-800x450.jpg"
+        ]
+    },
+    {
+        pageNumber: 2,
+        title: "Tip 1: Kies de juiste plek",
+        text: [
+            "Leg je moestuin aan op een plek met zowel zon als schaduw. ",
+            "Maak de ondergrond vlak, zodat regen- of sproeiwater goed opgenomen kan worden door de planten. ",
+            "Is het een winderige plek? Dan is het handig om je moestuin te omheinen met een groene haag. ",
+            "Een vak van 1,2 x 1,2 meter is genoeg om mee te beginnen.",
+            "[[img/aardbei5-800x450.jpg"
+        ]
+    },
+    {
+        pageNumber: 3,
+        title: "Tip 2: Goede grond",
+        text: [
+            "De kwaliteit van de grond is erg belangrijk voor het slagen van de oogst. ",
+            "[[img/Moestuin-800x450.jpg",
+            "Bereid de grond, het zaaibed, grondig voor. ",
+            "Woel de aarde goed om en verwijder de zo veel mogelijk de oude begroeiing. ",
+            "Zorg vooral dat alle ‘onkruidresten’ uit je zaaibed verdwenen zijn. ",
+            "De grond mag niet meer bevroren of té vochtig zijn. ",
+            "De zaadjes kunnen dan niet goed kiemen. ",
+            "Hark de bovenste laag goed los en egaliseer de grond. ",
+            "Hoog de plek zo nodig op met tuinaarde.",
+            "Zorg dat de bovenste 10 cm van het zaaibed bedekt is met moestuingrond.",
+            "[[img/Zaaien1-800x450.jpg"
+        ]
+    },
+    {
+        pageNumber: 4,
+        title: "Tip 3: Bemesting",
+        text: [
+            "Gebruik een niet te zware bemesting, daar kunnen kiemplantjes niet goed tegen. ",
+            "Een organische bemesting werkt voor een moestuin het beste. ",
+            "[[img/potato-800x450.jpg",
+            "Over het algemeen geldt dat hoe meer mest, hoe meer blad. ",
+            "Dat is voor bladgroenten natuurlijk gunstig, maar bijvoorbeeld wortelgroenten geven dan vooral veel loof, en weinig wortel.",
+            "Veel groenten houden er wel van om verwend te worden met wat compost."
+        ]
+    },
+    {
+        pageNumber: 5,
+        title: "Veel plezier met je moestuin!",
+        text: [
+            "[[img/Zaaien2-800x450.jpg",
+        ]
+    },
+
+];
+
