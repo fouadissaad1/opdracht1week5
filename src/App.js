c24ef953395a6fdf0308cc48effd87b1de107c7d
fouadissaad1 <71667992+fouadissaad1@users.noreply.github.com>
2021-05-16 21:07:20 +0200

    add css + data Mapping .

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { DATA } from "./data/data_v1";
 
+
+function Moestuin(props){
+  const {moestuin}=props;
+  return <div>
+       <h1>{moestuin.title}</h1>
+    <div>{moestuin.text}</div>
+
+    <footer>{moestuin.pageNumber}</footer>
+  </div>
+
+}
 function App() {
   return (
-    <div className="App">
+    <div>
+      {
+        DATA.map((p)=><Moestuin key={p.pageNumber} moestuin={p}/>)
+      }
 
     </div>
   );
