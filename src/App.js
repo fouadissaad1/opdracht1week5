c3adcb0fd15879e2c5f3ad225c52ebdc434bf376
fouadissaad1 <71667992+fouadissaad1@users.noreply.github.com>
2021-05-17 19:59:49 +0200

    modified function moestuin to Indexpage Add OderLine and funtion Text

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import './App.css';
 import { DATA } from "./data/data_v1";
 
-
-function Moestuin(props){
-  const {moestuin}=props;
+function OderLine() {
   return <div>
-       <h1>{moestuin.title}</h1>
-    <div>{moestuin.text}</div>
+    <hr/>
+  </div>;
+}
+function Text(props) {
+  const {text} = props;
+  if (text.startsWith("---")) return <OderLine/>;
+  return <div>{text}</div>;
+}
 
-    <footer>{moestuin.pageNumber}</footer>
+function IndexPage(props){
+  const {indexPage}=props;
+  return <div>
+       <h1>{indexPage.title}</h1>
+    <div>
+      {indexPage.text.map((t, i) => <Text key={i} text={t}/>)}
+    </div>
+    <footer>-{indexPage.pageNumber}-</footer>
   </div>
-
 }
 function App() {
   return (
     <div>
       {
-        DATA.map((p)=><Moestuin key={p.pageNumber} moestuin={p}/>)
+        DATA.map((p)=><IndexPage key={p.pageNumber} indexPage={p}/>)
       }
+
+
 
     </div>
   );
